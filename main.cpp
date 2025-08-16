53448fc06cc4e5619a63110c14d1c8c0807e1789
yuyanfei <yuyanfei2002@163.com>
2025-08-16 16:22:55 +0800

    加入获取聊天记录功能

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include <QPushButton>
 #include <ui_Server.h>
 #include <QIcon>
-
+#include <QDir>
+#include <QLibraryInfo>
 #include "server.h"
 
 int main(int argc, char* argv[]) {
+
+    QApplication::addLibraryPath(QCoreApplication::applicationDirPath() + "/plugins");
     QApplication a(argc, argv);
     QApplication::setWindowIcon(QIcon(":/icon/install.ico"));
+
+    qputenv("QT_PLUGIN_PATH", QCoreApplication::applicationDirPath().toUtf8() + "/plugins");
+
     Server server;
     server.show();
     return QApplication::exec();
